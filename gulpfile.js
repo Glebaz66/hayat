0536cd980b48beb1f5b5ca810ba0354ce12d9b64
morfiiius <morfiiius@gmail.com>
2020-08-28 18:20:00 +0300

    add src

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,53 @@
-let project_folder = 'dist',
-    source_folder  = 'src';
+let project_folder = 'dist';
+let sourse_folder = 'src';
 
 let path = {
     build: {
         html: project_folder + '/',
-        css: project_folder + '/css/',
-        js: project_folder + '/js/',
-        img: project_folder + '/img/',
-        fonts: project_folder + '/fonts/',
+        css: project_folder + '/css',
+        img: project_folder + '/img',
+        js: project_folder + '/js',
+        fonts: project_folder + '/fonts',
     },
     src: {
-        html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
-        css: source_folder + '/scss/**/style.scss',
-        js: source_folder + '/js/script.js',
-        img: source_folder + '/img/**/*.{jpg,png,svg,webp,gif,ico}',
-        fonts: source_folder + '/fonts/*.{ttf, woff,woff2}',
+        html: [sourse_folder + '/*.html', "!" + sourse_folder + '/_*.html'],
+        css: sourse_folder + '/scss/style.scss',
+        img: sourse_folder + '/img/**/*.{jpg,png,svg,gif,webp,ico}',
+        js: sourse_folder + '/js/script.js',
+        fonts: sourse_folder + '/fonts/**/*.*',
     },
     watch: {
-        html: source_folder + '/**/*.html',
-        css: source_folder + '/scss/**/style.scss',
-        js: source_folder + '/js/**/*.js',
-        img: source_folder + '/img/**/*.{jpg,png,svg,webp,gif,ico}',
+        html: sourse_folder + '/**/*.html',
+        css: sourse_folder + '/scss/**/*.scss',
+        img: sourse_folder + '/img/**/*.{jpg,png,svg,gif,webp,ico}',
+        js: sourse_folder + '/js/**/*.js',
     },
     clean: './' + project_folder + '/',
+    
 }
 
-let { src, dest } = require('gulp'),
-    gulp = require('gulp'),
-    browsersync = require('browser-sync').create(),
-    fileinclude = require('gulp-file-include'),
-    del = require('del'),
-    scss = require('gulp-sass'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss'),
-    group_media = require('gulp-group-css-media-queries'),
-    clean_css = require('gulp-clean-css'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify-es').default,
-    imagemin = require('gulp-imagemin'),
-    webp = require('gulp-webp');
-    // webp_html = require('gulp-webp-html'),
-    // webp_css = require('gulp-webpcss');
 
-function browserSync(params){
+let { src, dest } = require('gulp');
+
+let gulp = require('gulp');
+let browsersync = require('browser-sync').create();
+let fileinclude = require('gulp-file-include');
+let del = require('del');
+let scss = require('gulp-sass');
+let autoprefixer = require('autoprefixer');
+let group_media = require('gulp-group-css-media-queries');
+let clean_css = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let postcss = require('gulp-postcss');
+let uglify = require('gulp-uglify-es').default;
+let imagemin = require('gulp-imagemin');
+let webp = require('gulp-webp');
+let webp_html = require('gulp-webp-html');
+let webp_css = require('gulp-webp-css');
+// let svg_sprite = require('gulp-svg-sprite');
+
+
+function browserSync(params) {
     browsersync.init({
         server: {
             baseDir: './' + project_folder + '/',
@@ -52,17 +57,10 @@ function browserSync(params){
     })
 }
 
-function libs() {
-    return gulp.src([
-        source_folder + '/libs/bootstrap/css/*.*'
-    ])
-    .pipe(gulp.dest(project_folder + '/libs/bootstrap/css/'));
-    };
-
 function html(){
     return src(path.src.html)
+    .pipe(webp_html())
     .pipe(fileinclude())
-    // .pipe(webp_html())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream());
 }
@@ -71,22 +69,21 @@ function css(){
     return src(path.src.css)
     .pipe(
         scss({
-            outputStyle: 'expanded'
+            outputStyle: 'expanded',
         })
     )
-    .pipe(group_media())
+    .pipe(
+        group_media()
+    )
     .pipe(
         postcss([ 
             autoprefixer({
-                overrideBrowserslist: ['last 5 versions'],
-                cascade: true,
+                overrideBrowserslist: ['last 3 versions'],
+                cascade: true
             }) 
         ])
     )
-    // .pipe(webp_css({
-    //     webpClass: '.webp',
-    //     noWebpClass: '.no-webp',
-    // }))
+    .pipe(webp_css())
     .pipe(dest(path.build.css))
     .pipe(clean_css())
     .pipe(
@@ -95,14 +92,16 @@ function css(){
         })
     )
     .pipe(dest(path.build.css))
-    .pipe(browsersync.stream());
+    .pipe(browsersync.stream())
 }
 
 function js(){
     return src(path.src.js)
     .pipe(fileinclude())
     .pipe(dest(path.build.js))
-    .pipe(uglify())
+    .pipe(
+        uglify()
+    )
     .pipe(
         rename({
             extname: '.min.js',
@@ -116,19 +115,19 @@ function images(){
     return src(path.src.img)
     .pipe(
         webp({
-            quality: 70,
+            quality: 70
         })
     )
     .pipe(dest(path.build.img))
     .pipe(src(path.src.img))
     .pipe(
         imagemin({
-            interlaced: true,
             progressive: true,
+            interlaced: true,
             optimizationLevel: 3,
             svgoPlugins: [
                 {
-                    removeViewBox: false,
+                    removeViewBox: false
                 }
             ]
         })
@@ -137,25 +136,33 @@ function images(){
     .pipe(browsersync.stream());
 }
 
+// gulp.task('svg_sprite', function(){
+//     return gulp.src([sourse_folder + '/iconsprite/*.svg'])
+// })
+
+// function fonts(params){
+//     return src(path.src.fonts)
+//     .pipe(dest(path.build.fonts))
+// }
 function watchFiles(params){
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
     gulp.watch([path.watch.js], js);
     gulp.watch([path.watch.img], images);
+    // gulp.watch([path.watch.fonts], fonts);
 }
 
-function clean(params){
-    return del(path.clean);
+function clean(){
+    return del(path.clean)
 }
-
-let build = gulp.series(clean,libs, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-exports.libs = libs;
+// exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
-exports.build = build;
 exports.html = html;
+exports.build = build;
 exports.watch = watch;
 exports.default = watch;
